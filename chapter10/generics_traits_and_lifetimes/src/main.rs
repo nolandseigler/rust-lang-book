ac39fbd256ffe3ea90cd1f1d3fb705387fe05fd3
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2023-11-16 16:19:45 -0500

    wip

---
+++ chapter10/generics_traits_and_lifetimes/src/main.rs
@@ -0,0 +1,55 @@
+// fn largest(list: &[i32]) -> &i32 {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+
+    println!("{}", both_integer.x);
+    println!("{}", both_float.x);
+    println!("{}", integer_and_float.y);
+    println!("{}", integer_and_float.x());
+}
